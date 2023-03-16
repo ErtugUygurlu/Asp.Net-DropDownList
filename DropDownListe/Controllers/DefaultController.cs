192eb2489ea006862e6347db3253b2309e0b6401
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-16 14:38:57 +0300

    Add project files.

---
+++ DropDownListe/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DropDownListe.Models.ENTITY;
+
+namespace DropDownListe.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        dropdownEntities db = new dropdownEntities();
+        public ActionResult Index()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLSEHIR.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.SEHIR,
+                                                 Value = i.ID.ToString(),
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+    }
+}
